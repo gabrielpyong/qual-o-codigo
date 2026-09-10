24d1f60d0a4ec090a8d98bb4df3c9fb415d9ff39
gabrielpyong <contato.gabrielpyong@gmail.com>
2026-09-10 15:33:31 -0300

    Update products list with new items and modify tare

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,13 +1,13 @@
 import { Product } from "@/types/product";
 
 export const products: Product[] = [
-  { id: "patinho", name: "Patinho", code: "741", tare: 18, chamber: "D2" },
-  { id: "alcatra", name: "Alcatra", code: "612", tare: 20, chamber: "D2" },
-  { id: "coxao-mole", name: "Coxão Mole", code: "160", tare: 16, chamber: "D2" },
-  { id: "coxao-fora", name: "Coxão de Fora", code: "552", tare: 22, chamber: "D2" },
-  { id: "coxao-dentro", name: "Coxão de Dentro", code: "553", tare: 22, chamber: "D2" },
+  { id: "Patinho Ped", name: "Patinho", code: "741", tare: 12, chamber: "D2" },
+  { id: "Bife Picanha", name: "Picanha", code: "1079", tare: 8, chamber: "D3" },
+  { id: "Bife Coxa de dentro estancia", name: "bife coxao de dentro", code: "3838", tare: 16, chamber: "D2" },
+  { id: "Ossobuco", name: "Ossobuco", code: "7904", tare: 14, chamber: "D3" },
+  { id: "Coxao de dentro Pedaço", name: "Coxão de Dentro", code: "160", tare: 16, chamber: "D2" },
   { id: "pernil", name: "Pernil Suíno", code: "1817", tare: 14, chamber: "E3" },
-  { id: "contra-file", name: "Contrafilé", code: "620", tare: 20, chamber: "D3" },
-  { id: "costela", name: "Costela Bovina", code: "225", tare: 14, chamber: "D3" },
+  { id: "Agulha C/ Osso", name: "Agulha", code: "191", tare: 16, chamber: "D4" },
+  { id: "Costela", name: "Costela Bovina", code: "225", tare: 10, chamber: "D3" },
   { id: "frango", name: "Frango", code: "7599", tare: 20, chamber: "E4" },
 ];
