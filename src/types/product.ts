a9513350515337b3932e3e56841666116e0b4f6a
gabrielpyong <contato.gabrielpyong@gmail.com>
2026-09-10 16:20:43 -0300

    Add 'Rômulo' to ChamberCode type

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,4 +1,4 @@
-export type ChamberCode = "D1" | "D2" | "D3" | "D4" | "E1" | "E2" | "E3" | "E4"| "E5";
+export type ChamberCode = "D1" | "D2" | "D3" | "D4" | "E1" | "E2" | "E3" | "E4"| "E5"| "Rômulo";
 
 export interface Product {
   id: string;
